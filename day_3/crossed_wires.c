aa8ed2fecb82196a8fc857e8bc1fd5e0ee9fbf20
endus <markus.gulla@gmail.com>
2025-07-17 20:16:41 -0600

    Fixed memory issues

--- day_3/crossed_wires.c
+++ day_3/crossed_wires.c
@@ -14,9 +14,8 @@ FILE *input = fopen("wire_reading.txt", "r");
 
 uint32_t edge_length = 30000;
 uint32_t grid_size = edge_length * edge_length;
-Goat *goats = malloc(sizeof(Goat) * 3);
-uint16_t *map_one = malloc(sizeof(uint8_t) * grid_size);
-uint16_t *map_two = malloc(sizeof(uint8_t) * grid_size);
+uint16_t *map_one = malloc(sizeof(uint16_t) * grid_size);
+uint16_t *map_two = malloc(sizeof(uint16_t) * grid_size);
 uint16_t starting = edge_length/2;
 Vec2 pos_one = 
 {
@@ -33,8 +32,8 @@ uint16_t debug = 1;
 uint16_t steps = 0;
 uint8_t map = 1;
 char read, direction;
-memset(map_one,0,grid_size);
-memset(map_two,0,grid_size);
+memset(map_one,0,sizeof(uint16_t) * grid_size);
+memset(map_two,0,sizeof(uint16_t) * grid_size);
 
 // printf("GOOD\n");                       // testing
 while((read = fgetc(input)) != EOF)
@@ -61,25 +60,28 @@ while((read = fgetc(input)) != EOF)
             {
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_one[x_y[0][0]][x_y[0][1] - i] = 1;
+                    size_t index = ((pos_one.y - i) * edge_length) + pos_one.x;
+                    map_one[index] = 1;
                 }
-                x_y[0][1] -= steps;
+                pos_one.y -= steps;
             }
             if(direction == 'R')
             {
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_one[x_y[0][0] + i][x_y[0][1]] = 1;
+                    size_t index = (pos_one.y * edge_length) + (pos_one.x + i);
+                    map_one[index] = 1;
                 }
-                x_y[0][0] += steps;
+                pos_one.x += steps;
             }
             if(direction == 'L')
             {
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_one[x_y[0][0] - i][x_y[0][1]] = 1;
+                    size_t index = (pos_one.y * edge_length) + (pos_one.x - i);
+                    map_one[index] = 1;
                 }
-                x_y[0][0] -= steps;
+                pos_one.x -= steps;
             }
             steps = 0;
             // printf("Loop %d\n",debug++);            // testing
@@ -90,34 +92,37 @@ while((read = fgetc(input)) != EOF)
             {
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_two[x_y[1][0]][x_y[1][1] + i] = 1;
+                    size_t index = ((pos_two.y + i) * edge_length) + pos_two.x;
+                    map_two[index] = 1;
                 }
-                x_y[1][1] += steps;
+                pos_two.y += steps;
             }
             if(direction == 'D')
             {
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_two[x_y[1][0]][x_y[1][1] - i] = 1;
+                    size_t index = ((pos_two.y - i) * edge_length) + pos_two.x;
+                    map_two[index] = 1;
                 }
-                x_y[1][1] -= steps;
+                pos_two.y -= steps;
             }
             if(direction == 'R')
             {
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_two[x_y[1][0] + i][x_y[1][1]] = 1;
+                    size_t index = (pos_two.y * edge_length) + (pos_two.x + i);
+                    map_two[index] = 1;
                 }
-                x_y[1][0] += steps;
+                pos_two.x += steps;
             }
             if(direction == 'L')
             {
-               //  printf("STEPS ON LEFT 2: %d\n", steps);     // testing
                 for(int i = 1; i <= steps; i++)
                 {
-                    map_two[x_y[1][0] - i][x_y[1][1]] = 1;
+                    size_t index = (pos_two.y * edge_length) + (pos_two.x - i);
+                    map_two[index] = 1;
                 }
-                x_y[1][0] -= steps;
+                pos_two.x -= steps;
             }
             steps = 0;
             break;
@@ -133,42 +138,48 @@ while((read = fgetc(input)) != EOF)
         steps += read - '0';
     }
 }
-printf("Steps: %c%d\n", direction,steps);
-if(direction == 'U')
-{
-    for(int i = 1; i <= steps; i++)
-    {
-        map_two[x_y[1][0]][x_y[1][1] + i] = 1;
-    }
-    x_y[1][1] += steps;
-}
-if(direction == 'D')
-{
-    for(int i = 1; i <= steps; i++)
-    {
-        map_two[x_y[1][0]][x_y[1][1] - i] = 1;
-    }
-    x_y[1][1] -= steps;
-}
-if(direction == 'R')
-{
-    for(int i = 1; i <= steps; i++)
-    {
-        map_two[x_y[1][0] + i][x_y[1][1]] = 1;
-    }
-    x_y[1][0] += steps;
-}
-if(direction == 'L')
-{
-    //  printf("STEPS ON LEFT 2: %d\n", steps);     // testing
-    for(int i = 1; i <= steps; i++)
-    {
-        map_two[x_y[1][0] - i][x_y[1][1]] = 1;
-    }
-    x_y[1][0] -= steps;
-}
-fclose(input);
 
+
+if(direction == 'U')
+            {
+                for(int i = 1; i <= steps; i++)
+                {
+                    size_t index = ((pos_two.y + i) * edge_length) + pos_two.x;
+                    map_two[index] = 1;
+                }
+                pos_two.y += steps;
+            }
+            if(direction == 'D')
+            {
+                for(int i = 1; i <= steps; i++)
+                {
+                    size_t index = ((pos_two.y - i) * edge_length) + pos_two.x;
+                    map_two[index] = 1;
+                }
+                pos_two.y -= steps;
+            }
+            if(direction == 'R')
+            {
+                for(int i = 1; i <= steps; i++)
+                {
+                    size_t index = (pos_two.y * edge_length) + (pos_two.x + i);
+                    map_two[index] = 1;
+                }
+                pos_two.x += steps;
+            }
+            if(direction == 'L')
+            {
+                for(int i = 1; i <= steps; i++)
+                {
+                    size_t index = (pos_two.y * edge_length) + (pos_two.x - i);
+                    map_two[index] = 1;
+                }
+                pos_two.x -= steps;
+            }
+            steps = 0;
+
+fclose(input);
+printf("Read and mapped");
 // done reading and mapping
 
 // now finding closest intersection 
@@ -176,54 +187,47 @@ fclose(input);
 int16_t distance = 0;
 int8_t loop = 1;
 
-while(loop != 0){
+while(loop != 0)
+{
     for(int i = 0; i < distance; i++)
     {
-   //     printf("Map one: %d Map two: %d \n",map_one[starting + i][starting + distance - i], map_two[starting + i][starting + distance - i] );
-        printf("\nCoordinates: %d,%d\n",starting + i, starting + distance - i);
-        if(map_one[starting + i][starting + distance - i] == 1 && map_two[starting + i][starting + distance - i] == 1)
+        size_t index = ((pos_one.y + distance - i)) + (pos_one.x + i);
+        if(map_one[index] == map_two[index])
         {
-            printf("\nDISTANCE!!!!!!: %d\n", distance);
             loop = 0;
-        } 
+        }
     }
-    if(loop == 0) break;
+
     for(int i = 0; i < distance; i++)
     {
-        printf("Map one: %d Map two: %d \n",map_one[starting + distance - i][starting - i],map_two[starting + distance - i][starting - i] );
-        printf("\nCoordinates: %d,%d\n", starting + distance - i, starting - i);
-        if(map_one[starting + distance - i][starting - i] == 1 && map_two[starting + distance - i][starting - i] == 1) 
+        size_t index = ((pos_one.y + i)) + (pos_one.x + distance - i);
+        if(map_one[index] == map_two[index])
         {
-            printf("\nDISTANCE!!!!!!: %d\n", distance);
             loop = 0;
-            break;
-        } 
+        }        
     }
-    if(loop == 0) break;
+
     for(int i = 0; i < distance; i++)
     {
-   //     printf("Map one: %d Map two: %d \n",map_one[starting - i][starting - distance + i],map_two[starting - i][starting - distance + i]  );
-          printf("\nCoordinates: %d,%d\n", starting - i, starting - distance + i);
-        if(map_one[starting - i][starting - distance + i] == 1 && map_two[starting - i][starting - distance + i] == 1)
+        size_t index = ((pos_one.y - distance + i)) + (pos_one.x - i);
+        if(map_one[index] == map_two[index])
         {
-            printf("\nDISTANCE!!!!!!: %d\n", distance);
             loop = 0;
-            break;
-        } 
+        }        
     }
-    if(loop == 0) break;
+
     for(int i = 0; i < distance; i++)
     {
-     //   printf("Map one: %d Map two: %d \n", map_one[starting - distance + i][starting + i],  map_two[starting - distance + i][starting + i]);
-        printf("\nCoordinates: %d,%d\n", starting - distance + i, starting + i);
-        if(map_one[starting - distance + i][starting + i] == 1 && map_two[starting - distance + i][starting + i] == 1)
-        {   
-            printf("\nDISTANCE!!!!!!: %d\n", distance);
+        size_t index = ((pos_one.y - i)) + (pos_one.x - distance + i);
+        if(map_one[index] == map_two[index])
+        {
             loop = 0;
-            break;
-        } 
+        }        
     }
-    if(loop != 0) distance++;
+    distance++;
 }
+
+free(map_one);
+free(map_two);
 printf("\nDistance: %d\n", distance);
 }
